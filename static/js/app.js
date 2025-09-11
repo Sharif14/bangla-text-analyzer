4fc146961b4925ec4a147d309dfabf10d4edf685
Sharif14 <you@example.com>
2025-09-11 12:48:41 +0600

    app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,8 @@
-{
-    "builds" : [
-        {
-            "src" : "app.py",
-            "use" : "@vercel/python"
-        }
-    ],
-    "routes": [
-        "src" : "/(.*)",
-        "dest" : "/app.py"
-    ]
-}
+// উদাহরণ (React/Next.js থেকে):
+fetch("https://bangla-text-analyzer-yjkp.onrender.com/predict", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: "...." }),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
